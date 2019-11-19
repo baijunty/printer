3f4f7c20bb20ac5f369f4d52a5a9d309fd7cf4c1
bai <baijunty@163.com>
2019-11-19 11:39:59 +0800

    remove columus limit & update support lib

--- app/src/main/java/com/baijunty/printer/html/HtmlPrinter.kt
+++ app/src/main/java/com/baijunty/printer/html/HtmlPrinter.kt
@@ -1,12 +1,12 @@
 package com.baijunty.printer.html
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
 import android.print.PrintAttributes
 import android.print.PrintJob
 import android.print.PrintManager
-import android.support.annotation.RequiresApi
 import android.view.View
 import android.webkit.WebView
 import com.baijunty.printer.PrintWorkModel
@@ -18,7 +18,7 @@ import io.reactivex.schedulers.Schedulers
 /**
  * 使用[htmlWriter]生成内容
  */
-@RequiresApi(Build.VERSION_CODES.KITKAT)
+@TargetApi(Build.VERSION_CODES.KITKAT)
 class HtmlPrinter(private var htmlWriter: PrinterWriter): PrintWorkModel {
     override var writer: PrinterWriter
         get() = htmlWriter
